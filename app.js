e3862ec0d0e1e44f5f4c2231131ebff956993a65
Uhamzat105 <Uhamzat105@gmail.com>
2025-03-17 14:30:54 +0100

    Assignment submission

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-var day = "monday"
-var position = "1"
-var output = "today is monday. it is the first day of the week"
+const day = "monday"
+
 console.log(day)
-console.log(position)
-console.log(output)
